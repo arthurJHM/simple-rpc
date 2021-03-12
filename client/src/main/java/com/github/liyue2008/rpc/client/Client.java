b5b734dd6fb19fa698f5d03e9e0dc258d378541b
nightsswatch <arthurjihengming@gmail.com>
2021-03-12 14:52:38 +0800

    持续

--- client/src/main/java/com/github/liyue2008/rpc/client/Client.java
+++ client/src/main/java/com/github/liyue2008/rpc/client/Client.java
@@ -20,9 +20,12 @@ import com.github.liyue2008.rpc.spi.ServiceSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.crypto.Data;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
+import java.util.Timer;
 
 /**
  * @author LiYue
@@ -30,7 +33,7 @@ import java.net.URI;
  */
 public class Client {
     private static final Logger logger = LoggerFactory.getLogger(Client.class);
-    public static void main(String [] args) throws IOException {
+    public static void main(String [] args) throws IOException, InterruptedException {
         String serviceName = HelloService.class.getCanonicalName();
         File tmpDirFile = new File(System.getProperty("java.io.tmpdir"));
         File file = new File(tmpDirFile, "simple_rpc_name_service.data");
@@ -43,8 +46,12 @@ public class Client {
             logger.info("找到服务{}，提供者: {}.", serviceName, uri);
             HelloService helloService = rpcAccessPoint.getRemoteService(uri, HelloService.class);
             logger.info("请求服务, name: {}...", name);
-            String response = helloService.hello(name);
-            logger.info("收到响应: {}.", response);
+            String response;
+            while (true) {
+                response = helloService.hello(name + "" + new Date().getTime());
+                Thread.sleep(1000);
+                logger.info("收到响应: {}.", response);
+            }
         }
 
 
