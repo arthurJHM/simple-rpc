caf8ed34ace6422417cabb24612e9955decacea9
nightsswatch <arthurjihengming@gmail.com>
2021-03-12 21:30:51 +0800

    cglib注释

--- rpc-netty/src/main/java/com/github/simplerpc/rpc/client/CGLibDynamicProxy.java
+++ rpc-netty/src/main/java/com/github/simplerpc/rpc/client/CGLibDynamicProxy.java
@@ -13,13 +13,21 @@ public class CGLibDynamicProxy extends AbstractStub implements MethodInterceptor
 
     private Class clz;
 
-    public CGLibDynamicProxy(Class clz){
+    public CGLibDynamicProxy(Class clz) {
         this.clz = clz;
     }
 
     private Enhancer enhancer = new Enhancer();
 
-    public Object getProxy(){
+    /*生成代理类的常规代码
+        Enhancer enhancer=new Enhancer();
+
+        enhancer.setSuperclass(ConcreteClassNoInterface.class);
+
+        enhancer.setCallback(new ConcreteClassInterceptor());
+
+        ConcreteClassNoInterface ccni=(ConcreteClassNoInterface)enhancer.create();*/
+    public Object getProxy() {
         //设置需要创建子类的类
         enhancer.setSuperclass(clz);
         enhancer.setCallback(this);
@@ -27,8 +35,14 @@ public class CGLibDynamicProxy extends AbstractStub implements MethodInterceptor
         return enhancer.create();
     }
 
+    //    这个函数就是重点
+//    定义一个拦截器。在调用目标方法时，CGLib会回调MethodInterceptor接口方法拦截，
+//    来实现你自己的代理逻辑，类似于JDK中的InvocationHandler接口。
+//    参数：Object为由CGLib动态生成的代理类实例，Method为上文中实体类所调用的被代理的方法引用
+//    ，Object[]为参数值列表，MethodProxy为生成的代理类对方法的代理引用。
+//返回：从代理实例的方法调用返回的值。
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        return SerializeSupport.parse(invokeRemote(new RpcRequest(clz.getCanonicalName(),method.getName(),SerializeSupport.serialize(objects))));
+        return SerializeSupport.parse(invokeRemote(new RpcRequest(clz.getCanonicalName(), method.getName(), SerializeSupport.serialize(objects))));
     }
 }
